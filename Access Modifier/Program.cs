6a90037c6cd4a80ecd51d71fd7f9fd5419d4a6bd
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-14 14:43:58 +0530

    Fetched

---
+++ Access Modifier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Access_Md
+{
+    public class Program
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Public Method");
+        }
+        private void Test2()
+        {
+            Console.WriteLine("Private Method");
+        }
+        protected void Test3()
+        {
+            Console.WriteLine("Protected Method");
+        }
+        internal void Test4()
+        {
+            Console.WriteLine("Internal Method");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Program p1 = new Program();
+            p1.Test1();
+            p1.Test2();
+            p1.Test3();
+            p1.Test4();
+        }
+    }
+}
---
+++ Access Modifier/Two.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_Md
+{
+    public class Two:Program
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Two p1 = new Two();
+            p1.Test1();
+            p1.Test3();
+            p1.Test4();
+        }
+    }
+}
