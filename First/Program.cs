22c6470dcc9a44565ad91e8f2b57ea9ae5326a97
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-14 11:17:41 +0530

    Fetched

---
+++ First/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class First
+    {
+        public void studentData()
+        {
+            Console.WriteLine("This is first class file");
+        }
+    }
+
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            First first = new First();
+            first.studentData();
+        }
+    }
+}
---
+++ Hello_World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class First
+    {
+        public void studentData()
+        {
+            Console.WriteLine("This is first class file");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            First first = new First();
+            first.studentData();
+
+        }
+    }
+}
