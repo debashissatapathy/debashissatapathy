c82c3ec50a300e13e76f7a43f3a7797f1840f97a
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-14 15:46:44 +0530

    Br1-Main

--- gitPractice/Program.cs
+++ gitPractice/Program.cs
@@ -7,6 +7,8 @@ namespace gitPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Br1-Main");
         }
     }
 }
