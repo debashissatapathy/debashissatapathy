f71dff6d984ee5da31daba5683a4f65be9731433
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-18 13:49:45 +0530

    Fetched

---
+++ Employee_Wages/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Wages
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UC1 u = new UC1();
+            u.Attendance();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Employee_Wages/UC1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wages
+{
+    public class UC1
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int emp_check = random.Next(2);
+            if (emp_check == 1)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+
+        }
+    }
+
+}
